d037d3415582c9d3ffd5cce87fd13d68727eb397
Eshika rawat <eshikarawat02@gmail.com>
2024-02-24 13:00:44 +0530

    done with hero section

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,18 +1,18 @@
 import { Button, Paper, Typography } from '@material-ui/core';
-import React from 'react';
+import HeroImage from "../../assets/images/heroimage.png";
 
 const Hero = () => {
   return (
     <div className='herosection'>
         <div className='herofirstdiv'>
             <Paper className='heropaper'>
-                <Typography variant='h5'>Our Mission</Typography>
-                <Typography component={"p"}></Typography>
-                <Button>Discover Menu</Button>
+                <Typography className='herotypoh5' variant='h5'>Our Mission</Typography>
+                <Typography className='herotypop' component={"p"}>To make delicious, healthy food accessible for everyone, from students, to families, join us and enjoy the freedom of stress-free, flavourful meals</Typography>
+                <Button className='herobutton'>Discover Menu</Button>
             </Paper>
         </div>
         <div className='heroseconddiv'>
-            <img src='' />
+            <img src={HeroImage} alt='heroimage' />
         </div>
     </div>
   )
